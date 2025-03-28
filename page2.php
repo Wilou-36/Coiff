e8ed1e2c9e92c1dd0e787244ceced52e0ee6cb9a
Williams DO REGO <williams.dorego@lan.sio.lyceefulbert.fr>
2025-03-28 11:19:28 +0100

    maj2

---
+++ page2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+       <meta charset ="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>MyHair</title>
+       <link rel="icon" href="favicon.png" type="image/png">
+       <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Le Cluedo</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="test1.php">Accueil</a></li>
+                <li><a href="page2.php">Les règles</a></li>
+                <li><a href="page3.php">Start</a></li>
+            </ul>
+        </nav>
+        <p>
+           
+        </p>
+    </body>
+</html>
